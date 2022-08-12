7dea9739343deb89954f0fdbda1fcdd3aedb0323
katacoder48 <katacoder48@gmail.com>
2022-08-12 14:56:16 +0900

    add amount

--- contracts/custom/rewardPool/interfaces/IRewardLiquidityPoolManagerEvent.sol
+++ contracts/custom/rewardPool/interfaces/IRewardLiquidityPoolManagerEvent.sol
@@ -5,5 +5,5 @@ pragma solidity >=0.8.0;
 import "../../../interfaces/IConcentratedLiquidityPoolManager.sol";
 
 interface IRewardLiquidityPoolManagerEvent is IConcentratedLiquidityPoolManagerEvent {
-    event CollectReward(address indexed pool, address indexed recipient, uint256 indexed positionId, uint256 reward);
+    event CollectReward(address indexed pool, address indexed recipient, uint256 indexed positionId, uint256 amount);
 }
