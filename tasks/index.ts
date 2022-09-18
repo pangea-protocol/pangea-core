bd4f315236e642344a79815826b2fb55dfb0df62
katacoder48 <katacoder48@gmail.com>
2022-09-18 10:42:40 +0900

    handling `yarn build`

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,9 +1,13 @@
-import "./accounts";
-import "./addresses";
-import "./dashboard";
-import "./erc20";
-import "./pool";
-import "./position";
-import "./swap";
-import "./time";
-import "./yieldPool";
+import fs from "fs";
+
+if (fs.existsSync('types/index.ts')) {
+  import("./accounts");
+  import( "./addresses");
+  import("./dashboard");
+  import("./erc20");
+  import("./pool");
+  import("./position");
+  import("./swap");
+  import("./time");
+  import("./yieldPool");
+}
