963ef785a96152f411056af84d337235df217ab2
katacoder48 <katacoder48@gmail.com>
2023-01-02 17:17:13 +0900

    implement getRewards

--- contracts/dashboard/PositionDashboardV2.sol
+++ contracts/dashboard/PositionDashboardV2.sol
@@ -13,6 +13,8 @@
 
 pragma solidity >=0.8.0;
 
+import "../custom/miningPool/interfaces/IMiningPoolManager.sol";
+import "../custom/miningPool/interfaces/IMiningPool.sol";
 import "../interfaces/IConcentratedLiquidityPoolManager.sol";
 import "../libraries/DyDxMath.sol";
 import "../libraries/FullMath.sol";
@@ -20,7 +22,6 @@ import "../libraries/TickMath.sol";
 import "../libraries/FixedPoint.sol";
 
 contract PositionDashboardV2 {
-
     function getTotal(address poolManager, uint256 positionId) external view returns (uint256 amount0, uint256 amount1) {
         (amount0, amount1) = getPrincipal(poolManager, positionId);
         (uint256 fee0, uint256 fee1) = getFees(poolManager, positionId);
@@ -29,7 +30,9 @@ contract PositionDashboardV2 {
     }
 
     function getPrincipal(address poolManager, uint256 positionId) public view returns (uint256 amount0, uint256 amount1) {
-        IConcentratedLiquidityPoolManagerStruct.Position memory position = IConcentratedLiquidityPoolManager(poolManager).positions(positionId);
+        IConcentratedLiquidityPoolManagerStruct.Position memory position = IConcentratedLiquidityPoolManager(poolManager).positions(
+            positionId
+        );
 
         uint256 priceLower = uint256(TickMath.getSqrtRatioAtTick(position.lower));
         uint256 priceUpper = uint256(TickMath.getSqrtRatioAtTick(position.upper));
@@ -42,4 +45,9 @@ contract PositionDashboardV2 {
         (fee0, fee1, , ) = IConcentratedLiquidityPoolManager(poolManager).positionFees(positionId);
     }
 
+    function getRewards(address poolManager, uint256 positionId) public view returns (address rewardToken, uint256 rewardAmount) {
+        IConcentratedLiquidityPoolManagerStruct.Position memory position = IMiningPoolManager(poolManager).positions(positionId);
+        rewardToken = IMiningPool(position.pool).rewardToken();
+        (rewardAmount, ) = IMiningPoolManager(poolManager).positionRewardAmount(positionId);
+    }
 }
