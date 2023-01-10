b5587299be664eae726d2ccd2fb3440828925e8b
katacoder48 <katacoder48@gmail.com>
2023-01-10 16:13:09 +0900

    upgrade miningPoolV2 & yieldPoolV2

--- deploy/baobab/MiningPoolUpgrade.ts
+++ deploy/baobab/MiningPoolUpgrade.ts
@@ -1,82 +1,73 @@
-import {DeployFunction} from "hardhat-deploy/types";
-import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {doTransaction, waitConfirmations} from "../utils";
-import {MiningPoolFactory__factory, MiningPoolV2__factory} from "../../types";
-import {BigNumber} from "ethers";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { doTransaction, waitConfirmations } from "../utils";
+import { MiningPoolFactory__factory } from "../../types";
+import { BigNumber } from "ethers";
 
 const deployFunction: DeployFunction = async function ({
-                                                         network,
-                                                         deployments,
-                                                         getNamedAccounts,
-                                                         ethers,
-                                                       }: HardhatRuntimeEnvironment) {
-  const {deploy} = deployments;
-  const {dev} = await getNamedAccounts();
-  const deployer = await ethers.getNamedSigner('deployer');
-  const protocolFee = 2000;
+  deployments,
+  getNamedAccounts,
+  ethers,
+}: HardhatRuntimeEnvironment) {
+  const { deploy } = deployments;
+  const { dev } = await getNamedAccounts();
+  const deployer = await ethers.getNamedSigner("deployer");
 
   // [1] Upgrade MiningPoolV2
-  const {address: poolImplementation} = await deploy("MiningPoolV2", {
+  const { address: poolImplementation } = await deploy("MiningPoolV2", {
     from: deployer.address,
     libraries: {
-      RewardTicks: "0xfF8E5bDf54D5ab74D2E56CA0E575348A52B95B3d"
+      RewardTicks: "0xfF8E5bDf54D5ab74D2E56CA0E575348A52B95B3d",
     },
     log: true,
     waitConfirmations: await waitConfirmations(),
-    gasPrice: BigNumber.from("250000000000")
+    gasPrice: BigNumber.from("250000000000"),
   });
 
   // [2] Upgrade MiningPoolFactory
-  const {address: MiningPoolFactoryAddress} = await deploy("MiningPoolFactory", {
-    from: deployer.address,
-    proxy: {
-      owner: dev,
-      proxyContract: "OpenZeppelinTransparentProxy",
-      execute: {
-        init: {
-          methodName: "initialize",
-          args: [poolImplementation, "0x899d8Ff3d3BD16DBE4eFF245BdA27EF96C01044B", "0x6e66D3aDfc2902b9e0A46C80D2803642596cc5F6"]
-        }
+  const { address: MiningPoolFactoryAddress } = await deploy(
+    "MiningPoolFactory",
+    {
+      from: deployer.address,
+      proxy: {
+        owner: dev,
+        proxyContract: "OpenZeppelinTransparentProxy",
+        execute: {
+          init: {
+            methodName: "initialize",
+            args: [
+              poolImplementation,
+              "0x899d8Ff3d3BD16DBE4eFF245BdA27EF96C01044B",
+              "0x6e66D3aDfc2902b9e0A46C80D2803642596cc5F6",
+            ],
+          },
+        },
       },
-    },
-    log: true,
-    waitConfirmations: await waitConfirmations(),
-    gasPrice: BigNumber.from("250000000000")
-  });
+      log: true,
+      waitConfirmations: await waitConfirmations(),
+      gasPrice: BigNumber.from("250000000000"),
+    }
+  );
 
   // [3] check protocolFee
-  const miningPoolFactory = MiningPoolFactory__factory.connect(MiningPoolFactoryAddress, deployer);
-  await doTransaction(
-      miningPoolFactory.setDefaultProtocolFee(protocolFee)
-  );
-  console.log(`default protocolFee : ${await miningPoolFactory.defaultProtocolFee()}`);
-
-  // [4] set Pool Implementation
-  await doTransaction(
-      miningPoolFactory.setPoolImplementation(poolImplementation)
+  const miningPoolFactory = MiningPoolFactory__factory.connect(
+    MiningPoolFactoryAddress,
+    deployer
   );
 
   // [5] upgrade previous pools
   const pools = [
-    '0xc322b656c8A56b64a03004c2E0BC4fcEEB23C06f',
-    '0x23303Def910469dddbf9C5e2D11920418eB82edf',
-    '0xB1E6dD3624867fE60E6D638f1A7B857bDCC928E0'
-  ]
+    "0xc322b656c8A56b64a03004c2E0BC4fcEEB23C06f",
+    "0x23303Def910469dddbf9C5e2D11920418eB82edf",
+    "0xB1E6dD3624867fE60E6D638f1A7B857bDCC928E0",
+    "0x73997Da14B7f962474Bc934F8056D401C7391d56",
+    "0xe8Ad8A95349B22ec6F72Ab79C6efEb8602c492BB",
+    "0x3cF6d6825Bea2dA9C8B83a6756c8D7a694930857",
+    "0xA35504725c5559fE159c39a6203378e8418c90c4",
+    "0x6fd93c41F3348Ec82ddFD8f29531DfA558a85Ffd",
+  ];
 
-  const calls:string[] = [];
-  const functionData = MiningPoolV2__factory.createInterface().encodeFunctionData("setProtocolFee", [protocolFee]);
-  for (let i=0; i < pools.length; i++) {
-    calls.push(functionData);
-  }
-
-  await doTransaction(
-      miningPoolFactory.upgradePoolsAndCall(pools, calls)
-  )
-
-  for (const address of pools) {
-    const pool = await MiningPoolV2__factory.connect(address, ethers.provider);
-    console.log(`${address} : ${await pool.protocolFee()}`);
-  }
+  await doTransaction(miningPoolFactory.upgradePools(pools));
 };
 
 export default deployFunction;
